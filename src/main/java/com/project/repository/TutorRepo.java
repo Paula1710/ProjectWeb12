cc74f1c605331869289f993b6aba62b7660ede59
Galin <alberto.galindo@edu.uah.es>
2023-01-05 11:27:55 +0100

    Repos

---
+++ src/main/java/com/project/repository/TutorRepo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.project.repository;
+
+import com.project.model.Tutor;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ *
+ * @author Alberto Galindo
+ */
+public interface TutorRepo extends JpaRepository<Tutor,Integer> {
+    
+}
